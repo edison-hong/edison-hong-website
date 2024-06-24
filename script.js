2b14b654ffb9197be38b6169e819c69ec8dd4229
Edison Hong <107368285+edison-hong@users.noreply.github.com>
2024-06-24 12:06:17 -0400

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WHLT2FW67X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WHLT2FW67X');
+</script>
